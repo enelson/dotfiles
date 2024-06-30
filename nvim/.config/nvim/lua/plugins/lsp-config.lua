3a9a26e4f87cac09d731d3d65fb2b1388f7f407c
Eric Nelson <usr.nelson@gmail.com>
2024-06-30 17:02:19 -0600

    Updates

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -15,32 +15,71 @@ return {
     end
   },
   {
+    'hrsh7th/cmp-nvim-lsp',
+  },
+  {
+    'L3MON4D3/LuaSnip',
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    config = function()
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
+
+      luasnip.config.setup {}
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end
+        },
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        }, {
+          { name = 'buffer' },
+        }),
+        preselect = 'item',
+        completion = {
+          completeopt = 'menu,menuone,noinsert'
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<CR>'] = cmp.mapping.confirm({select = false}),
+        }),
+      })
+    end
+  },
+  {
     "neovim/nvim-lspconfig",
-    lazy = false,
     config = function()
       local lspconfig = require('lspconfig')
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.gopls.setup({
-        settings = {
-          gopls = {
-            analyses = {
-              unusedparams = true,
-            },
-            staticcheck = true,
-            gofumpt = true,
-          },
-        },
-      })
-      lspconfig.jdtls.setup({})
 
-      vim.keymap.set('n', 'K',  vim.lsp.buf.hover, {})
-      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-      vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, {})
-      vim.keymap.set('n', '<leader>ic', vim.lsp.buf.incoming_calls, {})
-      vim.keymap.set('n', '<leader>ds', vim.lsp.buf.document_symbol, {})
-      vim.keymap.set('n', '<leader>rf', vim.lsp.buf.references, {})
+      local on_attach = function(_, bufnr)
+        local attach_opts = { silent = true, buffer = bufnr }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, attach_opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, attach_opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, attach_opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, attach_opts)
+        vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, attach_opts)
+        vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, attach_opts)
+        vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, attach_opts)
+        vim.keymap.set('n', '<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, attach_opts)
+        vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, attach_opts)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, attach_opts)
+        vim.keymap.set('n', 'so', require('telescope.builtin').lsp_references, attach_opts)
+      end
+
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+      local servers = { 'gopls', 'lua_ls', 'tsserver', 'jdtls' }
+      for _, lsp in ipairs(servers) do
+        lspconfig[lsp].setup {
+          on_attach = on_attach,
+          capabilities = capabilities,
+        }
+      end
     end
   }
 }
