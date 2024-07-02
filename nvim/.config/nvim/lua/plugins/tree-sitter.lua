6dede8b1f404bc8492accb8bc8fe6d88582db72f
Eric Nelson <usr.nelson@gmail.com>
2024-07-01 21:51:12 -0600

    Updates

--- nvim/.config/nvim/lua/plugins/tree-sitter.lua
+++ nvim/.config/nvim/lua/plugins/tree-sitter.lua
@@ -1,13 +1,48 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local tsconfig = require('nvim-treesitter.configs')
-    tsconfig.setup({
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local tsconfig = require('nvim-treesitter.configs')
+      tsconfig.setup({
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+            },
+          },
+          lsp_interop = {
+            enable = true,
+            border = 'none',
+            floating_preview_opts = {},
+            peek_definition_code = {
+              ["<leader>df"] = "@function.outer",
+              ["<leader>dF"] = "@class.outer",
+            },
+          },
+          swap = {
+            enable = true,
+            swap_next = {
+              ["<leader>aa"] = "@parameter.inner",
+            },
+            swap_previous = {
+              ["<leader>AA"] = "@parameter.inner",
+            },
+          },
+        }
+      })
+    end
+  }
 }
 
