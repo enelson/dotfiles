98112991f1ba90731e02f5d6aa312fc66c696222
Eric Nelson <usr.nelson@gmail.com>
2024-07-12 14:12:44 -0600

    Updates

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -4,8 +4,22 @@ return {
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
-      { "<Leader>a", function() local harpoon = require("harpoon"); harpoon:list():add() end, desc = "" },
-      { "<C-e>", function() local harpoon = require("harpoon"); harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "" },
+      {
+        "<Leader>am",
+        function()
+          local harpoon = require("harpoon")
+          harpoon:list():add()
+        end,
+        desc = "Add Harpoon Mark"
+      },
+      {
+        "<C-e>",
+        function()
+          local harpoon = require("harpoon")
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "List Harpoon Marks"
+      },
     },
     opts = {},
   },
