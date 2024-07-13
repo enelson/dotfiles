9c83c29bc1874a2c20dbe1c46ab24383b3aa14f9
Eric Nelson <usr.nelson@gmail.com>
2024-07-13 13:08:18 -0600

    Updates

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -8,7 +8,10 @@ return {
     keys = {
       { "<Leader>ff", function() local builtin = require('telescope.builtin'); builtin.find_files() end, desc = "FZF Find Files" },
       { "<Leader>fg", function() local builtin = require('telescope.builtin'); builtin.live_grep() end, desc = "FZF Live Grep" },
-      { "<Leader>fb", function() local builtin = require('telescope.builtin'); builtin.buffers() end, desc = "Fuzzy Buffers" },
+      { "<Leader>fb", function() local builtin = require('telescope.builtin'); builtin.buffers() end, desc = "FZF Buffers" },
+      { "<Leader>fs", function() local builtin = require('telescope.builtin'); builtin.grep_string() end, desc = "FZF Grep String" },
+      { "<Leader>ft", function() local builtin = require('telescope.builtin'); builtin.treesitter() end, desc = "FZF Treesitter" },
+      { "<Leader>fc", function() local builtin = require('telescope.builtin'); builtin.commands() end, desc = "FZF Commands" },
     },
     opts = {
     },
